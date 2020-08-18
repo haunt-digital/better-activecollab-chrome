8ac09fcbf031c344db53f8e9a97608a1f44ac17c
AlexJHockley <alexjhockley@gmail.com>
2020-08-18 16:36:23 +1200

    Added basic handler for fetching task data.

--- content.js
+++ content.js
@@ -1,11 +1,22 @@
 // If we are being called at load, the start URL is correct.
 console.log('Welcome home.');
 const splitUrl = window.location.pathname.split('/');
+collateEstimates(splitUrl[1], splitUrl[3]);
 
 chrome.runtime.onMessage.addListener(
   function(request, sender, sendResponse) {
     // Listen for update messages from background.js
     if (request.message === 'urlChange') {
       console.log('CHANGE:', request);
+      collateEstimates(request.userID, request.projectID);
     }
 });
+
+function collateEstimates(userID, projectID) {
+  console.log('Estimation:', projectID, userID);
+  fetch(`https://app.activecollab.com/${userID}/projects/${projectID}`).then((r) =>
+    r.json().then((b) =>
+      console.log(b.tracked_time)
+    )
+  );
+}
