bb9f1531130e810f8cad72969d85eab065985421
AlexJHockley <alexjhockley@gmail.com>
2021-05-06 13:37:07 +1200

    Plugin now works for both Firefox and Chrome

--- background.js
+++ background.js
@@ -1,23 +1,21 @@
 // Add event listeners the extension needs
-chrome.runtime.onInstalled.addListener(function() {
+chrome.runtime.onInstalled.addListener(function () {
   // Declarative content API lets us use the poup we defined
-  chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
-    chrome.declarativeContent.onPageChanged.addRules([{
-      conditions: [new chrome.declarativeContent.PageStateMatcher({
-        pageUrl: {hostEquals: 'app.activecollab.com'},
-      })
-      ],
-        actions: [new chrome.declarativeContent.ShowPageAction()]
-    }]);
+  chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
+    if (changeInfo.status === 'complete' && tab.url.match("https://app.activecollab.com/*")) {
+      chrome.pageAction?.show(tabId);
+    } else {
+      chrome.pageAction?.hide(tabId);
+    }
   });
 });
 
 // Listen for changes from the ActiveCollab website
 chrome.tabs.onUpdated.addListener(
-  function(tabId, changeInfo, tab) {
+  function (tabId, changeInfo, tab) {
     // The URL has changed. Send the info to the tab / extension.
     if (changeInfo.url) {
-      chrome.tabs.sendMessage( tabId, {
+      chrome.tabs.sendMessage(tabId, {
         message: 'urlChange',
         url: changeInfo.url
       })
